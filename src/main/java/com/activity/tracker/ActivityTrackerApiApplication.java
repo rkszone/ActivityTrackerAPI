eacea2d284476663472e9a37574c83a36cfa0de6
Rahul Kumar Singh <rahul.kumar.singh@nl.abnamro.com>
2021-03-25 23:15:41 +0100

    initial commit

---
+++ src/main/java/com/activity/tracker/ActivityTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.activity.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivityTrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/activity/tracker/ActivityTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.activity.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
